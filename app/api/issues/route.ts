3be327e03082788b1f53698e8fb1c15eee8436e8
Mariela <smart.happy.face_2906@hotmail.com>
2025-01-16 23:49:14 +0200

    Error handling.

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -5,8 +5,8 @@ import { prisma } from "@/prisma/client";
 
 // Define the shape of the issue object
 const issueSchema = z.object({
-    title: z.string().min(3).max(255),
-    description: z.string().min(3),
+    title: z.string().min(3,'Title is required.').max(255),
+    description: z.string().min(3,'Description is required.'),
     // status: z.enum(["open", "closed", "in-progress"]),
 });
 
@@ -15,7 +15,7 @@ export async function POST(request: NextRequest) {
     const parsedIssues = issueSchema.safeParse(issues);
 
     if(!parsedIssues.success || !parsedIssues.data) {
-        return NextResponse.json({ error: "Invalid issue data when creating an issue" }, { status: 400 });
+        return NextResponse.json(parsedIssues.error.format(), { status: 400 });
     }
 
     const newIssue = await prisma.issue.create({
