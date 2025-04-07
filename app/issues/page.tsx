63b32a6902fd497b2f3cd1c2f92dd2d822cbcad6
Mariela <smart.happy.face_2906@hotmail.com>
2025-04-07 22:41:06 +0300

    Refactoring the imports.

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import {  Table } from "@radix-ui/themes";
-import Link from '../components/Link';
+import { IssueStatusBadge, Link } from '@/app/components';
 import { prisma } from '@/prisma/client';
-import IssueStatusBadge from '../components/IssueStatusBadge';
-import delay from 'delay'
+import { Table } from "@radix-ui/themes";
+
+// import IssueStatusBadge from '../components/IssueStatusBadge';
+// import Link from '../components/Link';
 import IssueButton from './IssueButton';
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000)
+ 
   return (
     <div>
       <IssueButton />
